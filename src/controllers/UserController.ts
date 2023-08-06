04070a7a4bf65ce67068d93a95de403abc480562
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-08-06 11:36:11 -0300

    Commit Create User
    Commit Create User

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+import { prisma } from '../database';
+
+export default {
+  async createUser(request: Request, response: Response) {
+    try {
+      const { name, email } = request.body;
+
+      const userExist = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      });
+
+      if (userExist) {
+        return response.json({
+          error: true,
+          message: 'Erro: Usuário já existe'
+        });
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email
+        }
+      })
+
+      return response.json({
+          error: false,
+          message: 'Usuário criado com sucesso!',
+          status: 1,
+          user
+        });
+    }
+    catch (error) {
+      return response.json({ message: error.message });
+    }
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import Express from 'express';
+import UserController from './controllers/UserController';
+
+const app = Express();
+app.use(Express.json());
+const PORT = 8000;
+
+app.get('/', (request, response) => {
+  return response.send({ message: 'Hello World' });
+})
+
+app.post('/createUser', UserController.createUser);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
